daed35da0b2eea593f9e90dcb0e39e1a2580a7b4
jeffrymorris <jeffrymorris@gmail.com>
2015-08-26 13:07:22 -0700

    Added linq2couchbase example

---
+++ Src/Couchbase.Linq2CouchbaseExample/Beer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Couchbase.Linq.Filters;
+using Newtonsoft.Json;
+
+namespace Couchbase.Linq2CouchbaseExample
+{
+    [EntityTypeFilter("beer")]
+    public class Beer
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("abv")]
+        public decimal Abv { get; set; }
+
+        [JsonProperty("ibu")]
+        public decimal Ibu { get; set; }
+
+        [JsonProperty("srm")]
+        public decimal Srm { get; set; }
+
+        [JsonProperty("upc")]
+        public decimal Upc { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("brewery_id")]
+        public string BreweryId { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("style")]
+        public string Style { get; set; }
+
+        [JsonProperty("category")]
+        public string Category { get; set; }
+
+        [JsonProperty("updated")]
+        public DateTime Updated { get; set; }
+    }
+}
+
+#region [ License information          ]
+
+/* ************************************************************
+ *
+ *    @author Couchbase <info@couchbase.com>
+ *    @copyright 2015 Couchbase, Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ * ************************************************************/
+
+#endregion
---
+++ Src/Couchbase.Linq2CouchbaseExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Couchbase.Configuration.Client;
+using Couchbase.Linq;
+
+namespace Couchbase.Linq2CouchbaseExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ClientConfiguration
+            {
+                Servers = new List<Uri> { new Uri("http://localhost:8091") }
+            };
+
+            ClusterHelper.Initialize(config);
+            var db = new DbContext(ClusterHelper.Get(), "beer-sample");
+            var query = from b in db.Query<Beer>()
+                        select b;
+
+            foreach (var beer in query)
+            {
+                Console.WriteLine(beer.Type + ": " + beer.Name);
+            }
+            Console.Read();
+            ClusterHelper.Close();
+        }
+    }
+}
