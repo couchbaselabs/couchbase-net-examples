16470598049e9a5144a19ccf11615bac5eb4ea2b
jeffrymorris <jeffrymorris@gmail.com>
2015-09-21 18:31:57 -0700

    CAS and mgmt views and design docs

---
+++ Src/Couchbase.Examples.Managment.CreateUpdateViews/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Couchbase.Configuration.Client;
+
+namespace Couchbase.Examples.Managment.CreateUpdateViews
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var cluster = new Cluster())
+            {
+                using (var bucket = cluster.OpenBucket())
+                {
+                    var manager = bucket.CreateManager("Administrator", "");
+                    var designDoc = "{\"views\":{\"all_docs\":{\"map\":\"function (doc, meta) { emit(meta.id, doc); }\"}}}";
+                    var insertResult = manager.InsertDesignDocumentAsync("dd_docs", designDoc).Result;
+
+                    if (insertResult.Success)
+                    {
+                        var designDocModified = "{\"views\": {}}";
+                        var updateResult = manager.UpdateDesignDocumentAsync("dd_docs", designDocModified).Result;
+                        Console.WriteLine(updateResult.Success);
+                    }
+                }
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Src/Couchbase.Examples.OptimisticConcurrencyWithCas/Program.cs
@@ -0,0 +1,76 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Couchbase.Configuration.Client;
+using Couchbase.IO;
+
+namespace Couchbase.Examples.OptimisticConcurrencyWithCas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClusterHelper.Initialize(new ClientConfiguration
+            {
+                Servers = new List<Uri>
+                {
+                    new Uri("http://localhost:8091/")
+                }
+            });
+            var bucket = ClusterHelper.GetBucket("default");
+
+            var revision0 = new Post
+            {
+                PostId = "p0001",
+                Author = "Stuie",
+                Published = new DateTime(2012, 1, 13),
+                Content = "A really cool blog post!",
+                Views = 0
+            };
+
+            //insert the first revision
+            bucket.Upsert(revision0.PostId, revision0);
+
+            Task.Run(() => UpdatePostWithCasAsync(revision0));
+
+            Console.Read();
+            ClusterHelper.Close();
+        }
+
+        static async Task<bool> UpdatePostWithCasAsync(Post modified)
+        {
+            var bucket = ClusterHelper.GetBucket("default");
+            const int maxAttempts = 10;
+            var attempts = 0;
+
+            do
+            {
+                //get the original document - if it doesn't exist fail
+                var result = await bucket.GetAsync<Post>(modified.PostId);
+                if (result.Status == ResponseStatus.KeyNotFound)
+                {
+                    return false;
+                }
+
+                //update the original documents fields
+                var original = result.Value;
+                original.Content = modified.Content;
+                original.Author = modified.Author;
+                original.Views = original.Views++;
+
+                //perform the mutation passing in the CAS value
+                result = await bucket.UpsertAsync(modified.PostId, modified, result.Cas);
+                if (result.Success)
+                {
+                    return true;
+                }
+
+                //failed so try again up until maxAttempts
+            } while (attempts++ < maxAttempts);
+
+            //we failed within maxAttemps
+            return false;
+        }
+    }
+}
